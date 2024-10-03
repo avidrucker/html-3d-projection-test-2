7caa707be56405f9c3af8cc36ac1dd5d412284a6
avidrucker <avidrucker@gmail.com>
2024-10-03 12:46:18 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// src/App.jsx
+
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Simple HTML Content */}
+      <h1>Hello World</h1>
+      <p>This is a simple paragraph.</p>
+      <button>Click Me</button>
+      <a href="https://www.example.com">Visit Example.com</a>
+    </div>
+  );
+}
+
+export default App;
